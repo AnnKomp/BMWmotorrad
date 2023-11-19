258a39337643f4c517c38d1296cac5f1d85bc1df
simon.goy <simon.goy@etu.univ_smb.fr>
2023-11-19 12:13:53 +0000

    Div description enlevée

--- bmwmottorad/resources/views/moto-list.blade.php
+++ bmwmottorad/resources/views/moto-list.blade.php
@@ -23,6 +23,9 @@
         {{ $moto->nommoto }}
         </div>
         <img src={{$moto->lienmedia}} width=100% height=100%>
+        <div >
+            {{ $moto->descriptifmoto }}
+            </div>
 
 
     </div>
