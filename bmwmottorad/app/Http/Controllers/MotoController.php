5121c7de80140508c791c8f2419e208491af25f3
simon.goy <simon.goy@etu.univ_smb.fr>
2023-11-20 01:06:19 +0000

    Affichage des caractéristiques des équipements

--- bmwmottorad/app/Http/Controllers/MotoController.php
+++ bmwmottorad/app/Http/Controllers/MotoController.php
@@ -9,23 +9,49 @@ use App\Models\Moto;
 class MotoController extends Controller
 {
     public function index() {
-        $ranges = DB::table('modelemoto')->select('*')->join('gammemoto','modelemoto.idgamme','=','gammemoto.idgamme')->get();
-        $motos = DB::table('modelemoto')->select('*')->join('media', 'media.idmoto','=','modelemoto.idmoto')->whereColumn('idmediapresentation','idmedia')->get();
-        return view ("moto-list", ['motos'=>$motos],['ranges'=>$ranges]);
+        $ranges = DB::table('modelemoto')
+            ->select('*')
+            ->join('gammemoto','modelemoto.idgamme','=','gammemoto.idgamme')
+            ->get();
+        $motos = DB::table('modelemoto')
+            ->select('*')
+            ->join('media', 'media.idmoto','=','modelemoto.idmoto')
+            ->whereColumn('idmediapresentation','idmedia')
+            ->get();
+        return view ("moto-list", ['motos'=>$motos, 'ranges'=>$ranges]);
     }
 
     public function detail(Request $request ) {
         $idmoto = $request->input('id');
-        $moto_infos = DB::table('caracteristique')->select('*')->join('categoriecaracteristique', 'categoriecaracteristique.idcatcaracteristique','=','caracteristique.idcatcaracteristique')->where('idbrochure','=',$idmoto)->get();
-        $moto_pics = DB::table('media')->select('lienmedia')->where('idmoto','=',$idmoto)->get();
-        return view ("moto", ["infos" => $moto_infos], ["moto_pics" => $moto_pics]);
+        $moto_infos = DB::table('modelemoto')
+            ->select('*')
+            ->join('caracteristique','caracteristique.idbrochure','=','modelemoto.idmoto')
+            ->join('categoriecaracteristique', 'categoriecaracteristique.idcatcaracteristique','=','caracteristique.idcatcaracteristique')
+            ->where('caracteristique.idbrochure','=',$idmoto)
+            ->get();
+        $moto_pics = DB::table('media')
+            ->select('lienmedia')
+            ->where('idmoto','=',$idmoto)
+            ->get();
+        $moto_options = DB::table('specifie')
+            ->select('*')
+            ->join('option','option.idoption','=','specifie.idoption')
+            ->where('specifie.idmoto','=',$idmoto)
+            ->get();
+        return view ("moto", ["infos" => $moto_infos, "moto_pics" => $moto_pics,"moto_options" => $moto_options]);
     }
 
     public function filter(Request $request) {
         $moto_range = $request->input('id');
-        $ranges = DB::table('modelemoto')->select('*')->join('gammemoto','modelemoto.idgamme','=','gammemoto.idgamme')->get();
-        $motos = DB::table('modelemoto')->select('*')->join('media', 'media.idmoto','=','modelemoto.idmoto')->whereColumn('idmediapresentation','idmedia')->where('modelemoto.idgamme', '=', $moto_range)->get();
-        return view("moto-list-filtered", ["motos" => $motos], ['ranges'=>$ranges]);
+        $ranges = DB::table('modelemoto')
+            ->select('*')->join('gammemoto','modelemoto.idgamme','=','gammemoto.idgamme')
+            ->get();
+        $motos = DB::table('modelemoto')
+            ->select('*')->join('media', 'media.idmoto','=','modelemoto.idmoto')
+            ->whereColumn('idmediapresentation','idmedia')
+            ->where('modelemoto.idgamme', '=', $moto_range)
+            ->get();
+        return view("moto-list-filtered", ["motos" => $motos, 'ranges'=>$ranges]);
     }
 
 
--- bmwmottorad/resources/views/moto.blade.php
+++ bmwmottorad/resources/views/moto.blade.php
@@ -1,3 +1,14 @@
+@php
+    $categories = [];
+
+    // Compter le nombre de lignes par catégorie
+    foreach ($infos as $info) {
+        $categories[$info->nomcatcaracteristique] = isset($categories[$info->nomcatcaracteristique])
+            ? $categories[$info->nomcatcaracteristique] + 1
+            : 1;
+    }
+@endphp
+
 @extends('layouts.menus')
 
 @section('title', 'Moto')
@@ -6,6 +17,7 @@
 
 
 @section('content')
+
 <div class="slider-container">
 <div class = 'slider'>
 @foreach ($moto_pics as $pic)
@@ -13,6 +25,7 @@
 @endforeach
 </div></div>
 
+{{-- Beginning of the part for the slider --}}
 <script type="text/javascript" src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
 
@@ -25,21 +38,47 @@
     });
 </script>
 
+{{-- End of the part for the slider --}}
+
+@if (!empty($infos))
+    <?php $motoname = $infos[0]->nommoto; ?>
+    <h1>La BMW {{ $motoname }}</h1>
+@endif
+
+
 <p><hr NOSHADE  ALIGN=CENTER WIDTH="40%" SIZE='5'></p>
 
 
 <h1>Fiche technique</h1>
 <table>
 <tr style='border: solid'>
-    <th class='category_caracteristics'>Catégorie </th>
-    <th style="font-size: 2em; border: solid; text-align= center">Caractéristique</th>
-    <th class='caracteristics'>Description</th>
+    <th class='top_caracteristics'>Catégorie </th>
+    <th  class='top_caracteristics'>Caractéristique</th>
+    <th class='top_caracteristics'>Description</th>
 </tr>
+
 @foreach ($infos as $info)
+    <tr >
+        @if ($categories[$info->nomcatcaracteristique] > 0)
+            <td class='category_caracteristics' rowspan="{{ $categories[$info->nomcatcaracteristique] }}">
+                {{ $info->nomcatcaracteristique }}
+            </td>
+            @php
+                $categories[$info->nomcatcaracteristique] = 0;
+            @endphp
+        @endif
+        <td class='caracteristics_name'>{{ $info->nomcaracteristique }}</td>
+        <td class='caracteristics'>{{ $info->valeurcaracteristique }}</td>
+    </tr>
+@endforeach
+</table>
+
+
+<table>
+@foreach ($moto_options as $option)
 <tr>
-<td class='category_caracteristics'>{{ $info->nomcatcaracteristique }}</td>
-<td class='caracteristics_name'>{{ $info->nomcaracteristique }}</td>
-<td class='caracteristics'>{{  $info->valeurcaracteristique }}</td>
+    <td class='caracteristics_name'>{{ $option->nomoption }}</td>
+    <td class='caracteristics'>{{ $option->detailoption }}</td>
 </tr>
 @endforeach
 </table>
