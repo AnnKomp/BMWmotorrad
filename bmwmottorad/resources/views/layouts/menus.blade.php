97252593225a96c173c45177f5499573ffd77db9
simon.goy <simon.goy@etu.univ_smb.fr>
2023-11-20 14:27:05 +0000

    Correction bug logo bmw + boutton login

--- bmwmottorad/resources/views/layouts/menus.blade.php
+++ bmwmottorad/resources/views/layouts/menus.blade.php
@@ -16,9 +16,14 @@
     <body>
 
     	<header>
-            <a href="{{ url("/") }}" class='menus'><img src="https://www.bmw-motorrad.fr/content/dam/bmwmotorradnsc/common/mnm/graphics/bmw_motorrad_logo.svg.asset.1585209612412.svg"></a>
+            <a href="{{ url("/") }}" class='menus'>
+                <img class="header-image" src="https://www.bmw-motorrad.fr/content/dam/bmwmotorradnsc/common/mnm/graphics/bmw_motorrad_logo.svg.asset.1585209612412.svg">
+            </a>
             <a href="{{ url("/motos") }}" class='menus'>Les motos</a>
             <a href="{{ url("/motos") }}" class='menus'>Les équipements</a>
+            <a href="{{ url("/login") }}" class='menus'>
+                <img class="login" src="https://www.bmw-motorrad.fr/etc.clientlibs/mnm/mnmnsc/clientlibs/global/resources/images/new/customer-portal-login.svg">
+            </a>
     	</header>
 
         <div>@yield('categories')</div>
@@ -26,6 +31,5 @@
         <div class="container">
             @yield('content')
         </div>
-
     </body>
 </html>
