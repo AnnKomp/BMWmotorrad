504300af218445079f3df625ba7974e69abde2bd
simon.goy <simon.goy@etu.univ_smb.fr>
2023-11-24 13:29:43 +0000

    Selectio de la couleur

--- bmwmottorad/resources/views/moto-color.blade.php
+++ bmwmottorad/resources/views/moto-color.blade.php
@@ -12,29 +12,45 @@
 @section('categories')
     <div class = 'header_category'>
         <a href="/moto?id={{$idmoto}}" class = "categories">{{ $motoname }}</a>
-        <a href="/moto/color?idmoto={{$idmoto}}&idcouleur={{$idcouleur}}" class = "categories">Couleurs</a>
-        <a href="/moto/pack?id={{$idmoto}}" class = "categories">Packs</a>
+        <a href="/moto/color?idmoto={{$idmoto}}&idcouleur={{$idcouleur}}" class = "categories">Configuration</a>
+{{--        <a href="/moto/pack?id={{$idmoto}}" class = "categories">Packs</a>--}}
     </div>
 @endsection
 
 
 @section('content')
-<div class="couleur">
-    @if ($idcouleur == 0 )
-    <img class="moto_color" src="{{ $motos[0]->lienmedia }}">
-    @else
-    <img class="moto_color" src="{{ $source[0]->motocouleur }}">
-    @endif
-    <table class="couleur">
-        @foreach ($moto_colors as $color)
-        <tr>
-            <td class="couleur"><a href="/moto/color?idmoto={{$idmoto}}&idcouleur={{ $color->idcouleur }}"><img src="{{$color->photocouleur}}"></a></td>
-            <td class="couleur">{{ $color->nomcouleur }}</td>
-            <td class="pack">{{ $color->prixcouleur }} €</td>
-            <td class="couleur"><a href="/color?idmoto={{$idmoto}}&idcouleur={{ $color->idcouleur }}" style="font-size:24px"><i class="fa fa-info-circle"></i></a></td>
-        </tr>
-        @endforeach
 
-    </table>
-</div>
+<form action="{{ route('processColors')}}?id={{$idmoto}}" method="post" >
+    @csrf
+    <div class="couleur">
+        <table class="lanceconfig">
+            <tr><td class="lanceconfig">
+            @if ($idcouleur == 0 )
+                <div><img class="moto_color" src="{{ $motos[0]->lienmedia }}"></div>
+            @else
+            <div><img class="moto_color" src="{{ $source[0]->motocouleur }}"></div>
+            @endif
+                <div>
+                    <button id="lancerconfig" type="submit">Lancer la configuration</button>
+                </div>
+            </td></tr>
+        </table>
+        <table class="couleur">
+            @foreach ($moto_colors as $color)
+            <tr>
+                <td><input type="radio" id="option1" name="color[]" value={{ $color->idcouleur }} checked @if(in_array($color->idcouleur, session('selectedColor', []))) checked @endif></td>
+                <td class="couleur"><a href="/moto/color?idmoto={{$idmoto}}&idcouleur={{ $color->idcouleur }}"><img src="{{$color->photocouleur}}"></a></td>
+                <td class="couleur">{{ $color->nomcouleur }}</td>
+                <td class="pack">{{ $color->prixcouleur }} €</td>
+                <td class="couleur"><a href="/color?idmoto={{$idmoto}}&idcouleur={{ $color->idcouleur }}" style="font-size:24px"><i class="fa fa-info-circle"></i></a></td>
+            </tr>
+            @endforeach
+
+        </table>
+
+
+    </div>
+</form>
+
+
 @endsection
