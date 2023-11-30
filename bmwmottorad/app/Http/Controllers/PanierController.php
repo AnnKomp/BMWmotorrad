ca07b79ca68fe4fd86ba5587d0ae932431a253aa
anna.kompaniets <anna.kompaniets@etu.univ-smb.fr>
2023-11-30 14:07:16 +0000

    on a le panier !!! mais PB avec quantités

--- bmwmottorad/app/Http/Controllers/PanierController.php
+++ bmwmottorad/app/Http/Controllers/PanierController.php
@@ -9,6 +9,26 @@ use Illuminate\Support\Facades\DB;
 
 class PanierController extends Controller
 {
+    public function index(Request $request){
+        $cart = $request->session()->get('cart', []);
+
+        $equipements = Equipement::whereIn('idequipement', array_keys($cart))->get();
+
+        return view('panier', compact('equipements','cart'));
+
+    }
+
+    public function addToCart(Request $request, $id){
+        $cart = $request->session()->get('cart', []);
+
+        $cart[$id] = isset($cart[$id]) ? $cart[$id] +1 : 1;
+
+        $request->session()->put('cart', $cart);
+
+        return redirect()->back()->with('succes', 'Equipement');
+
+    }
+
 
     public function info(Request $request) {
         $idpanier = $request->input('id');
